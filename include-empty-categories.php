59ff890cc88fdbf4dcb7218366fc1150e87f6584
farshad4004 <farshad4004@gmail.com>
2024-11-29 21:41:09 +0330

    Add files via upload

---
+++ include-empty-categories.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Include Empty Categories in Yoast Sitemap
+ * Plugin URI:  https://yourwebsite.com
+ * Description: این پلاگین دسته‌بندی‌های خالی محصولات ووکامرس را در نقشه سایت Yoast قرار می‌دهد.
+ * Version:     1.0
+ * Author:      Farshad
+ * Author URI:  https://acharwp.com
+ * License:     GPL-2.0+
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // جلوگیری از دسترسی مستقیم به فایل
+}
+
+add_filter('wpseo_sitemap_exclude_taxonomy', function ($value, $taxonomy) {
+    if ($taxonomy === 'product_cat') {
+        return false; // جلوگیری از حذف دسته‌بندی محصولات
+    }
+    return $value;
+}, 10, 2);
